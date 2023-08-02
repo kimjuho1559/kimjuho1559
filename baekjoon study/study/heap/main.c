ed263b9f80223adc78b82aef32fd5bbb076896ce
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-08-02 17:37:47 +0900

    그래프 (인접행렬) 1260

--- baekjoon study/study/heap/main.c
+++ baekjoon study/study/heap/main.c
@@ -1,10 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 #define TRUE 1
 #define FALSE 0
-#define MAX 10
 
+#define MAX 1000
+#define MAXE 10000
+
+int visited[MAX];
+int visited1[MAX];
 typedef int element;
 typedef struct {
 	element queue[MAX];
@@ -44,26 +49,22 @@ element dequeue(QueueType* q) {
 	return q->queue[q->front];
 }
 
-#define MAX_V 50
 typedef struct GraphType {
-	int n;
-	int adj_mat[MAX][MAX];
+	int n; // 정점의 개수
+	int adj_mat[MAX][MAXE];
 }GraphType;
-int visited[MAX];
-
 // 그래프 초기화
-void graph_init(GraphType* g) {
+void init(GraphType* g) {
 	int r, c;
 	g->n = 0;
 	for (r = 0; r < MAX; r++)
-		for (c = 0; c < MAX; c++)
+		for (c = 0; c < MAXE; c++)
 			g->adj_mat[r][c] = 0;
 }
-
 // 정점 삽입 연산
 void insert_vertex(GraphType* g, int v) {
 	if (((g->n) + 1) > MAX) {
-		fprintf(stderr, "그래프: 정점 개수 초과");
+		fprintf(stderr, "그래프: 정점의 개수 초과");
 		return;
 	}
 	g->n++;
@@ -78,21 +79,32 @@ void insert_edge(GraphType* g, int start, int end) {
 	g->adj_mat[end][start] = 1;
 }
 
+void dfs_mat(GraphType* g, int v) {
+	int w;
+	visited[v] = TRUE;
+	printf("%d ", v+1);
+	for (w = 0; w < g->n; w++)
+	{
+		if (g->adj_mat[v][w] && !visited[w])
+			dfs_mat(g, w); // 정점 w에서 DFS 새로 시작
+	}
+}
+
 void bfs_mat(GraphType* g, int v) {
 	int w;
 	QueueType q;
 
 	queue_init(&q);
-	visited[v] = TRUE; // 정점 v 방문 표시
-	printf("%d 방문-> ", v);
+	visited1[v] = TRUE; // 정점 v 방문 표시
+	printf("%d ", v+1);
 	enqueue(&q, v);
 	while (!is_empty(&q)) {
 		v = dequeue(&q); // 큐에 정점 추출
 		for (w = 0; w < g->n; w++) // 인접 정점 탐색
 		{
-			if (g->adj_mat[v][w] && !visited[w]) {
-				visited[w] = TRUE; // 방문 표시
-				printf("%d 방문-> ", w);
+			if (g->adj_mat[v][w] && !visited1[w]) {
+				visited1[w] = TRUE; // 방문 표시
+				printf("%d ", w+1);
 				enqueue(&q, w);
 			}
 		}
@@ -100,23 +112,21 @@ void bfs_mat(GraphType* g, int v) {
 }
 
 int main() {
+	int N, M, V;
 	GraphType* g;
 	g = (GraphType*)malloc(sizeof(GraphType));
-	graph_init(g);
-	for (int i = 0; i < 6; i++)
-	{
+	init(g);
+	scanf("%d %d %d", &N, &M, &V);
+	for (int i = 0; i < N; i++)
 		insert_vertex(g, i);
+	for (int i = 0; i < M; i++)
+	{
+		int u, v;
+		scanf("%d %d", &u, &v);
+		insert_edge(g, u-1,v-1);
 	}
-	insert_edge(g, 0, 2);
-	insert_edge(g, 2, 1);
-	insert_edge(g, 2, 3);
-	insert_edge(g, 0, 4);
-	insert_edge(g, 4, 5);
-	insert_edge(g, 1, 5);
-
-	printf("너비 우선 탐색\n");
-	bfs_mat(g, 0);
+	dfs_mat(g, V - 1);
 	printf("\n");
+	bfs_mat(g, V - 1);
 	free(g);
-	return 0;
 }
