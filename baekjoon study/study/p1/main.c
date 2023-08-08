18b7a13d9fc16f71fa6300fa1b74a56a556a7d12
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-08-08 20:37:45 +0900

    Floyd 알고리즘

--- baekjoon study/study/p1/main.c
+++ baekjoon study/study/p1/main.c
@@ -3,120 +3,57 @@
 
 #define TRUE 1
 #define FALSE 0
-#define MAX 10
+#define MAX 100
+#define INF 1000000
 
-typedef int element;
-typedef struct {
-	element queue[MAX];
-	int front, rear;
-}QueueType;
-
-// 오류 함수
-void error(char* message) {
-	fprintf(stderr, "%s\n", message);
-	exit(1);
-}
-
-// 초기화 함수
-void queue_init(QueueType* q) {
-	q->front = q->rear = 0;
-}
-
-int is_empty(QueueType* q) {
-	return (q->front == q->rear);
-}
-
-int is_full(QueueType* q) {
-	return ((q->rear + 1) % MAX == q->front);
-}
-
-void enqueue(QueueType* q, element item) {
-	if (is_full(q))
-		error("큐가 포화상태입니다");
-	q->rear = (q->rear + 1) % MAX;
-	q->queue[q->rear] = item;
-}
-
-element dequeue(QueueType* q) {
-	if (is_empty(q))
-		error("큐가 공백상태입니다");
-	q->front = (q->front + 1) % MAX;
-	return q->queue[q->front];
-}
-
-#define MAX_V 50
 typedef struct GraphType {
 	int n;
-	int adj_mat[MAX][MAX];
+	int weight[MAX][MAX];
 }GraphType;
-int visited[MAX];
 
-// 그래프 초기화
-void graph_init(GraphType* g) {
-	int r, c;
-	g->n = 0;
-	for (r = 0; r < MAX; r++)
-		for (c = 0; c < MAX; c++)
-			g->adj_mat[r][c] = 0;
-}
+int A[MAX][MAX];
 
-// 정점 삽입 연산
-void insert_vertex(GraphType* g, int v) {
-	if (((g->n) + 1) > MAX) {
-		fprintf(stderr, "그래프: 정점 개수 초과");
-		return;
-	}
-	g->n++;
-}
-// 간선 삽입 연산
-void insert_edge(GraphType* g, int start, int end) {
-	if (start >= g->n || end >= g->n) {
-		fprintf(stderr, "그래프: 정점 번호 오류");
-		return;
-	}
-	g->adj_mat[start][end] = 1;
-	g->adj_mat[end][start] = 1;
-}
-
-void bfs_mat(GraphType* g, int v) {
-	int w;
-	QueueType q;
-
-	queue_init(&q);
-	visited[v] = TRUE; // 정점 v 방문 표시
-	printf("%d 방문-> ", v);
-	enqueue(&q, v);
-	while (!is_empty(&q)) {
-		v = dequeue(&q); // 큐에 정점 추출
-		for (w = 0; w < g->n; w++) // 인접 정점 탐색
+void printA(GraphType* g) {
+	printf("*****************************\n");
+	for (int i = 0; i < g->n; i++)
+	{
+		for (int j = 0; j < g->n; j++)
 		{
-			if (g->adj_mat[v][w] && !visited[w]) {
-				visited[w] = TRUE; // 방문 표시
-				printf("%d 방문-> ", w);
-				enqueue(&q, w);
-			}
+			if (A[i][j] == INF)
+				printf("  * ");
+			else
+				printf("%3d ", A[i][j]);
 		}
+		printf("\n");
+	}
+	printf("*****************************\n");
+}
+
+void floyd(GraphType* g) {
+	for (int i = 0; i < g->n; i++) {
+		for (int j = 0; j < g->n; j++)
+			A[i][j] = g->weight[i][j];
+		printA(g);
+	}
+	for (int i = 0; i < g->n; i++) {
+		for (int j = 0; j < g->n; j++)
+			for (int k = 0; k < g->n; k++)
+				if (A[i][k] + A[k][j] < A[i][j])
+					A[i][j] = A[i][k] + A[k][j];
+		printA(g);
 	}
 }
 
 int main() {
-	GraphType* g;
-	g = (GraphType*)malloc(sizeof(GraphType));
-	graph_init(g);
-	for (int i = 0; i < 6; i++)
-	{
-		insert_vertex(g, i);
-	}
-	insert_edge(g, 0, 2);
-	insert_edge(g, 2, 1);
-	insert_edge(g, 2, 3);
-	insert_edge(g, 0, 4);
-	insert_edge(g, 4, 5);
-	insert_edge(g, 1, 5);
-
-	printf("너비 우선 탐색\n");
-	bfs_mat(g, 0);
-	printf("\n");
-	free(g);
+	GraphType g = { 7,
+		{ {0,7,INF,INF,3,10,INF},
+		{7,0,4,10,2,6,INF},
+		{INF,4,0,2,INF,INF,INF},
+		{INF,10,2,0,11,9,4},
+		{3,2,INF,11,0,INF,5},
+		{10,6,INF,9,INF,0,INF},
+		{INF,INF,INF,4,5,INF,0} }
+	};
+	floyd(&g);
 	return 0;
 }
