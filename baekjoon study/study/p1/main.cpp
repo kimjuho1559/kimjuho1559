4eab3ea96ec04a0ff7a4376d8f4840d53270bc5b
juho_kim <120544630+kimjuho1559@users.noreply.github.com>
2023-09-12 16:25:44 +0900

    sort 알고리즘 vector<,<pair<int int>>

--- baekjoon study/study/p1/main.cpp
+++ baekjoon study/study/p1/main.cpp
@@ -1,48 +1,34 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
+bool compare(pair<int, int> a, pair<int, int> b) {
+	return a.first < b.first;
+}
+
+
 int main() {
-	int H, W;
-	cin >> H >> W;
-	char arr[100][100];
-//	char** arr = new char*[H];
-//	for (int i = 0; i < H; i++)
-//	{
-//		*arr = new char[W];
-//	}
-	for (int i = 0; i < H; i++)
+	int N;
+	cin >> N;
+	vector<pair<int, int>> v;
+	for (int i = 0; i < N; i++)
 	{
-		cin >> arr[i];
+		int a, b;
+		cin >> a >> b;
+		v.push_back(make_pair(a, b));
 	}
-	for (int i = 0; i < H; i++)
+	sort(v.begin(), v.end(), compare);
+	int total = v[0].first + v[0].second;
+	for (int i = 1; i < N; i++)
 	{
-		int count = 0;
-		bool num = false;
-		for (int j = 0; j < W; j++)
-		{
-			if (arr[i][j] == 'c') {
-				num = true;
-				count = 0;
-				arr[i][j] = count;
-				count++;
-			}
-			else if (arr[i][j] == '.') {
-				if (num == true) {
-					arr[i][j] = count;
-					count++;
-				}
-				else
-					arr[i][j] = -1;
-			}
+		if (total < v[i].first) {
+			total = v[i].first;
+			total += v[i].second;
 		}
+		else
+			total += v[i].second;
 	}
-	for (int i = 0; i < H; i++)
-	{
-		for (int j = 0; j < W; j++)
-		{
-			cout << (int)arr[i][j] << " ";
-		}
-		cout << endl;
-	}
+	cout << total;
 }
